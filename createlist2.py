232d90f67235f973903ec09e39548079b136d229
BHARAT KUMAR PRAJAPAT <123304396+bharatprajapat014@users.noreply.github.com>
2023-02-15 12:46:13 +0530

    Create createlist2.py

---
+++ createlist2.py
@@ -0,0 +1,14 @@
+ #Creating a list with multiple distinct or duplicate elements
+  # Creating a List with
+# the use of Numbers
+# (Having duplicate values)
+List = [1, 2, 4, 4, 3, 3, 3, 6, 5]
+print("\nList with the use of Numbers: ")
+print(List)
+
+# Creating a List with
+# mixed type of values
+# (Having numbers and strings)
+List = [1, 2, 'bharat', 4, 'kumar', 6, 'prajapat']
+print("\nList with the use of Mixed Values: ")
+print(List)
