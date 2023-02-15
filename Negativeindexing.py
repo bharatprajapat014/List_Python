f56cf02b62a8f79bce00e3a989fffccf1887e02c
BHARAT KUMAR PRAJAPAT <123304396+bharatprajapat014@users.noreply.github.com>
2023-02-15 12:48:54 +0530

    Create Negativeindexing.py

---
+++ Negativeindexing.py
@@ -0,0 +1,11 @@
+List = [1, 2, 'Bharat', 4, 'kumar', 6, 'prajapat']
+
+# accessing an element using
+# negative indexing
+print("Accessing element using negative indexing")
+
+# print the last element of list
+print(List[-1])
+
+# print the third last element of list
+print(List[-3])
