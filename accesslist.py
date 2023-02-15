600d3c03f42452bbe150dc6e79b9ae692aba5663
BHARAT KUMAR PRAJAPAT <123304396+bharatprajapat014@users.noreply.github.com>
2023-02-15 12:47:11 +0530

    Create accesslist.py

---
+++ accesslist.py
@@ -0,0 +1,13 @@
+#Accessing elements from the List
+# Python program to demonstrate
+# accessing of element from list
+
+# Creating a List with
+# the use of multiple values
+List = ["BHARAT", "KUMAR", "PRAJAPAT"]
+
+# accessing a element from the
+# list using index number
+print("Accessing a element from the list")
+print(List[0])
+
