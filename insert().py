8464b2ce6c0850af287d27a3f7dff04226e2ce73
BHARAT KUMAR PRAJAPAT <123304396+bharatprajapat014@users.noreply.github.com>
2023-02-15 12:56:10 +0530

    Create insert().py

---
+++ insert().py
@@ -0,0 +1,16 @@
+#method2
+# Python program to demonstrate
+# Addition of elements in a List
+
+# Creating a List
+List = [1,2,3,4]
+print("Initial List: ")
+print(List)
+
+# Addition of Element at
+# specific Position
+# (using Insert Method)
+List.insert(3, 12)
+List.insert(0, 'bharat')
+print("\nList after performing Insert Operation: ")
+print(List)
