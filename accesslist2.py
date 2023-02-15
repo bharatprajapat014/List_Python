ec30bc188a6dede7ec227c1d2d3e060cc0722f3b
BHARAT KUMAR PRAJAPAT <123304396+bharatprajapat014@users.noreply.github.com>
2023-02-15 12:48:12 +0530

    Create accesslist2.py

---
+++ accesslist2.py
@@ -0,0 +1,11 @@
+#Accessing elements from a multi-dimensional list
+# Creating a Multi-Dimensional List
+# (By Nesting a list inside a List)
+List = [['Bharat', 'Kumar'], ['Prajapat']]
+
+# accessing an element from the
+# Multi-Dimensional List using
+# index number
+print("Accessing a element from a Multi-Dimensional list")
+print(List[0][1])
+print(List[1][0])
