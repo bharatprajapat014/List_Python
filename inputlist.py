d23f5039d73adc6e88517ecf4eb127e748e318bb
BHARAT KUMAR PRAJAPAT <123304396+bharatprajapat014@users.noreply.github.com>
2023-02-15 12:50:20 +0530

    Create inputlist.py

---
+++ inputlist.py
@@ -0,0 +1,12 @@
+#Taking Input of a Python List
+# Python program to take space
+# separated input as a string
+# split and store it to a list
+# and print the string list
+
+# input the list as string
+string = input("Enter elements (Space-Separated): ")
+
+# split the strings and store it to a list
+lst = string.split()
+print('The list is:', lst) # printing the list
